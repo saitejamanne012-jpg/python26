348f533ea04513f8f8f7fdf8b02a2d5c1ff482ba
saitejamanne012-jpg <saitejamanne012@gmail.com>
2025-12-12 11:22:28 +0530

    Add files via upload
    in reverse order input output

---
+++ 10.py
@@ -0,0 +1,17 @@
+# Write a program to display the number in reverse order
+# input : 6324
+# output: 4236
+
+
+# input: 6324  ---
+# output: 4236
+
+num=6324
+rev=0
+while num!=0:            # 6!=0
+    rem=num%10               # 6%10   =6
+    rev=rev*10+rem       #4236
+    num=num//10              # 6//10  6
+print("reverse number: ",rev)
+
+
